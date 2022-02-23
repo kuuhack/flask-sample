a938a56db4fee4c419f7a743093a314092a0db0a
taiga-tech <taiga.dev.n@gmail.com>
2022-02-24 03:59:35 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,18 +8,17 @@ import requests
 app = Flask(__name__)
 
 # URLを指定。URLにリクエストが来ると関数が実行される
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def index(path):
-    """
-    html = res.text
-    sorp = BeautifulSoup(html, 'html.parser')
-    title = sorp.find('title')
-    return title.text
-    """
-    print(path)
-    res = requests.get('https://api.github.com/users/taiga-tech')
-    return res.json()
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def index(path):
+#     """
+#     html = res.text
+#     sorp = BeautifulSoup(html, 'html.parser')
+#     title = sorp.find('title')
+#     return title.text
+#     """
+#     res = requests.get('https://api.github.com/users/taiga-tech')
+#     return res.json()
 
 @app.route('/search')
 def analyzer():
