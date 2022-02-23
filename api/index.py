f4cd6bc0da3952cc6cd5de72186a8a844b1e91b1
taiga-tech <taiga.dev.n@gmail.com>
2022-02-24 03:56:49 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request # Flaskパッケージをインポート
+# Flaskパッケージをインポート
+from flask import Flask, request, Response
 import requests
 # from bs4 import BeautifulSoup
 
-app = Flask(__name__) # Flaskクラスのインスタンス生成
+# Flaskクラスのインスタンス生成
+app = Flask(__name__)
 
-@app.route('/') # URLを指定。URLにリクエストが来ると関数が実行される
-def index():
+# URLを指定。URLにリクエストが来ると関数が実行される
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     """
     html = res.text
     sorp = BeautifulSoup(html, 'html.parser')
     title = sorp.find('title')
     return title.text
     """
+    print(path)
     res = requests.get('https://api.github.com/users/taiga-tech')
     return res.json()
 
