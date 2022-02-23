8999d7e881f0effc467acf72ad2284fcbd75cb04
taiga-tech <taiga.dev.n@gmail.com>
2022-02-24 04:37:08 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 # Flaskパッケージをインポート
-from flask import Flask, request # , Response
-# import requests
+from flask import Flask, request , Response
+import requests
 # from bs4 import BeautifulSoup
 
 # Flaskクラスのインスタンス生成
 app = Flask(__name__)
 
 # URLを指定。URLにリクエストが来ると関数が実行される
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-# def index(path):
+@app.route('/')
+def index():
 #     '''
 #     html = res.text
 #     sorp = BeautifulSoup(html, 'html.parser')
 #     title = sorp.find('title')
 #     return title.text
 #     '''
-#     res = requests.get('https://api.github.com/users/taiga-tech')
-#     return res.json()
-def analyzer(path):
-    print(path)
+    res = requests.get('https://api.github.com/users/taiga-tech')
+    return res.json()
+
+@app.route('/search')
+def analyzer():
     query = ''
     if request.args.get('q') is not None:
         query = request.args.get('q')
     else:
         query = 'パラメーターがないよ'
-    return query
+    return Response(query, mimetype='text/plain')
 
 # @app.route('/favicon.ico')
 # def favicon():
