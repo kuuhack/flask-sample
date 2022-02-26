0e7f687c26e90db58d5ab01aaf8e770f5df54f5f
taiga-tech <taiga.dev.n@gmail.com>
2022-02-26 23:07:56 +0900

    Update app.py

--- api/app.py
+++ api/app.py
@@ -3,20 +3,21 @@
 from flask import Flask, request , Response
 import requests
 # from bs4 import BeautifulSoup
+# from .gh import ghu
 
 # Flaskクラスのインスタンス生成
 app = Flask(__name__)
 
 # URLを指定。URLにリクエストが来ると関数が実行される
-@app.route('/')
-def index():
-#     '''
-#     html = res.text
-#     sorp = BeautifulSoup(html, 'html.parser')
-#     title = sorp.find('title')
-#     return title.text
-#     '''
-    res = requests.get('https://api.github.com/users/taiga-tech')
+@app.route('/<username>')
+def index(username: str):
+    '''
+    html = res.text
+    sorp = BeautifulSoup(html, 'html.parser')
+    title = sorp.find('title')
+    return title.text
+    '''
+    res = requests.get(f'https://api.github.com/users/{username}')
     return res.json()
 
 @app.route('/search')
