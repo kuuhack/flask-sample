9b4f2e0027305ee4dfe6b73bfe6c1fb7397d3736
taiga-tech <taiga.dev.n@gmail.com>
2022-03-01 11:05:23 +0900

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Flaskパッケージをインポート
 from flask import Flask, render_template
-
+import requests
 # import controllers
-from controllers import gh, search, favicon
+# from controllers import gh, search, favicon
 
 # Flaskクラスのインスタンス生成
 app = Flask(__name__)
@@ -12,12 +12,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# /api/vi/<username>
-app.register_blueprint(gh.app)
-# /api/v1/search?q=<query>
-app.register_blueprint(search.app)
+@app.route('/<username>')
+def ghu(username: str):
+    '''
+    html = res.text
+    sorp = BeautifulSoup(html, 'html.parser')
+    title = sorp.find('title')
+    return title.text
+    '''
+    res = requests.get(f'https://api.github.com/users/{username}')
+    return res.json()
 
-app.register_blueprint(favicon.app)
+# # /api/vi/<username>
+# app.register_blueprint(gh.app)
+# # /api/v1/search?q=<query>
+# app.register_blueprint(search.app)
+
+# app.register_blueprint(favicon.app)
 
 if __name__ == '__main__':
 	app.run()
